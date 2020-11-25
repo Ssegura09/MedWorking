486f453e60604599fd225d21a3cc44cd3a179f0f
Ssegura09 <stephaniemsegura09@gmail.com>
2020-11-25 16:49:44 -0600

    delete and add posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 // import Spinner from '../layout/Spinner';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -46,6 +52,50 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// DELETE post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert('Post removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// add post
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Post created', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,10 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -23,18 +29,18 @@ export default function (state = initialState, action) {
     //     post: payload,
     //     loading: false,
     //   };
-    // case ADD_POST:
-    //   return {
-    //     ...state,
-    //     posts: [payload, ...state.posts],
-    //     loading: false,
-    //   };
-    // case DELETE_POST:
-    //   return {
-    //     ...state,
-    //     posts: state.posts.filter((post) => post._id !== payload),
-    //     loading: false,
-    //   };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
