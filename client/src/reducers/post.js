a6fc8616af4471588f3f2b4a3a0f3796b882f6b7
Ssegura09 <stephaniemsegura09@gmail.com>
2020-12-01 16:20:31 -0600

    posts working again! :)

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,9 +1,12 @@
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
 
 const initialState = {
@@ -23,12 +26,12 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
-    // case GET_POST:
-    //   return {
-    //     ...state,
-    //     post: payload,
-    //     loading: false,
-    //   };
+    case GET_POST:
+      return {
+        ...state,
+        post: payload,
+        loading: false,
+      };
     case ADD_POST:
       return {
         ...state,
@@ -55,23 +58,23 @@ export default function (state = initialState, action) {
         ),
         loading: false,
       };
-    // case ADD_COMMENT:
-    //   return {
-    //     ...state,
-    //     post: { ...state.post, comments: payload },
-    //     loading: false,
-    //   };
-    // case REMOVE_COMMENT:
-    //   return {
-    //     ...state,
-    //     post: {
-    //       ...state.post,
-    //       comments: state.post.comments.filter(
-    //         (comment) => comment._id !== payload
-    //       ),
-    //     },
-    //     loading: false,
-    //   };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+        },
+        loading: false,
+      };
     default:
       return state;
   }
